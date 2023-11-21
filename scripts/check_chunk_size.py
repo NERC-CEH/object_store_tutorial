2caf30e4265d91ead75f920dde4247a68043980f
achevuturi <41617337+achevuturi@users.noreply.github.com>
2023-11-21 08:58:56 +0000

    Delete scripts/check_chunk_size.py

--- scripts/check_chunk_size.py
+++
@@ -1,8 +0,0 @@
-import dask.array as dsa
-
-zarrpath = (
-    "/gws/nopw/j04/ceh_generic/matbro/chess_scape/monthchunk/ens01-monthchunk/tas.zarr"
-)
-varname = "tas"
-
-dsa.from_zarr(zarrpath + "/" + varname)
