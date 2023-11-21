8e3ac3ab9446c6ffb2d80d4250f76a785a121a32
achevuturi <41617337+achevuturi@users.noreply.github.com>
2023-11-21 08:58:39 +0000

    Delete scripts/batchscript_gen_example.sh

--- scripts/batchscript_gen_example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for var in {hurs,huss,pr,psurf,rlds,rsds,sfcWind,tasmax,tasmin,tas}
-  do for ensmem in {01,04,06,15}
-    do cp -v chessscape_lotus_job_template 10kmmonthchunk/sbatch_scripts/chessscape_lotus_job_ensmem_${ensmem}_${var}.sh
-    sed -i "s/template1/${ensmem}/" 10kmmonthchunk/sbatch_scripts/chessscape_lotus_job_ensmem_${ensmem}_${var}.sh
-    sed -i "s/template2/${var}/" 10kmmonthchunk/sbatch_scripts/chessscape_lotus_job_ensmem_${ensmem}_${var}.sh
-  done
-done
